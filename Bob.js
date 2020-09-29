ccd48746fbf5792755843a49239a37e6bd4d56f7
beastly19802 <69489254+beastly19802@users.noreply.github.com>
2020-09-29 17:25:46 -0400

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,27 @@
+class Bob{
+    constructor(x, y, r) {
+        var options = {
+            'restitution':1,
+            'friction':0,
+            'density':0.8
+        }
+        
+        this.x = x
+        this.y = y
+        this.r = r
+
+        
+
+        this.body = Bodies.circle(this.x,this.y,(this.r)/2,options);
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        ellipseMode(CENTER);
+        ellipse(0, 0, this.r,this.r);
+        pop();
+      }
+}
